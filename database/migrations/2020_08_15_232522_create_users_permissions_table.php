d8c791f42b0773695e6bbdb94751aef22b6c9a42
danurwijayanto <mobinity.fx@gmail.com>
2020-08-16 06:33:26 +0700

    Add migration

---
+++ database/migrations/2020_08_15_232522_create_users_permissions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_permissions', function (Blueprint $table) {
+            // $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('permission_id');
+
+            // Foreign key constraint
+            $table->foreign('user_id')->reference('id')->on('users')->onDelete('cascade');
+            $table->foreign('permission_id')->reference('id')->on('permission')->onDelete('cascade');
+
+            //SETTING THE PRIMARY KEYS
+            $table->primary(['user_id', 'role_id']);
+            $table->timestamps();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_permissions');
+    }
+}
