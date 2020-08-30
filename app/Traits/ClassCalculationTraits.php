faf4102703558729e9e218dc262df9f013c92f76
danurwijayanto <mobinity.fx@gmail.com>
2020-08-30 13:42:38 +0700

    Add pembagian kelas

--- app/Traits/ClassCalculationTraits.php
+++ app/Traits/ClassCalculationTraits.php
@@ -11,7 +11,8 @@ trait ClassCalculationTraits
     {
         $doStudentClassOrder = $this->studentClassOrder();
         $doAssignClass = $this->assignClass($doStudentClassOrder);
-        return $doAssignClass;
+        $insertDb = $this->calculationInsertDb($doAssignClass);
+        return $insertDb;
     }
 
     private function studentClassOrder()
@@ -37,7 +38,6 @@ trait ClassCalculationTraits
                     'vector' => $dataSiswa[$i]['vektor_v3']
                 ],
             ];
-            // arsort($lintasMinatOrder);
             usort($lintasMinatOrder, function ($a, $b) {
                 return $b['vector'] <=> $a['vector'];
             });
@@ -67,7 +67,7 @@ trait ClassCalculationTraits
 
         // Check max class quota
         for ($i = 0; $i < count($dataMapel); $i++) {
-            $dataMapel[$i]['kuota_kelas'] = $dataMapel[$i]['jumlah_kelas'] * $dataMapel[$i]['kuota_kelas'];
+            $dataMapel[$i]['max_kuota_kelas'] = $dataMapel[$i]['jumlah_kelas'] * $dataMapel[$i]['kuota_kelas'];
             $dataMapel[$i]['kuota_kelas_terpakai'] = 0;
         }
 
@@ -75,35 +75,98 @@ trait ClassCalculationTraits
 
         for ($i = 0; $i < count($data); $i++) {
             $mapelSelected = 0;
-            // for ($j = 0; $j < count($data[$i]['urutan_lintas_minat']); $j++) {
+
             $idMakul1 = array_search($data[$i]['urutan_lintas_minat'][0]['mapel_id'], array_column($dataMapel, 'id'));
             $idMakul2 = array_search($data[$i]['urutan_lintas_minat'][1]['mapel_id'], array_column($dataMapel, 'id'));
-            // }
-            \Illuminate\Support\Facades\Log::debug($idMakul1);
-            if ($dataMapel[$idMakul1]['kuota_kelas_terpakai'] <  $dataMapel[$idMakul1]['kuota_kelas']){
+
+            if ($dataMapel[$idMakul1]['kuota_kelas_terpakai'] <  $dataMapel[$idMakul1]['max_kuota_kelas']) {
                 $mapelSelected = $data[$i]['urutan_lintas_minat'][0]['mapel_id'];
                 $dataMapel[$idMakul1]['kuota_kelas_terpakai']++;
-            }elseif($dataMapel[$idMakul2]['kuota_kelas_terpakai'] <  $dataMapel[$idMakul2]['kuota_kelas']){
+            } elseif ($dataMapel[$idMakul2]['kuota_kelas_terpakai'] <  $dataMapel[$idMakul2]['max_kuota_kelas']) {
                 $mapelSelected = $data[$i]['urutan_lintas_minat'][1]['mapel_id'];
                 $dataMapel[$idMakul2]['kuota_kelas_terpakai']++;
-            }else{
-                // \Illuminate\Support\Facades\Log::debug("Kelas ".$dataMapel[$idMakul1]['kode_mapel']." telah penuh untuk siswa ".$data[$i]['nama_siswa']);
-                \Illuminate\Support\Facades\Log::debug("Siswa ".$data[$i]['nama_siswa']." harus memilih kelas 3 atau sekolah harus menambahkan kuota kelas");
+            } else {
+                \Illuminate\Support\Facades\Log::debug("Siswa " . $data[$i]['nama_siswa'] . " harus memilih kelas 3 atau sekolah harus menambahkan kuota kelas");
             }
             $data[$i]['mapel_terpilih'] = $mapelSelected;
-            // Mencari index data mapel
-            // if (array_search($data[$i]['urutan_lintas_minat']['pilih_lm1'], array_column($dataMapel, 'nama_mapel'))){
-
-            // };
-
-            // $studentStatus = [
-            //     'kelas' => ,
-            //     'status' =>
-            // ];
-
-            // $data[$i]['status_murid'] = $studentStatus;
         }
-        \Illuminate\Support\Facades\Log::debug($dataMapel);
+
+        $returnData = [
+            'data_siswa' => $data,
+            'data_mapel' => $dataMapel
+        ];
+
         \Illuminate\Support\Facades\Log::debug($data);
+        return ($returnData);
+        // \Illuminate\Support\Facades\Log::debug($data);
+    }
+
+    private function calculationInsertDb($data)
+    {
+        if (empty($data)) {
+            return json_encode(['fail' => 'Class data is null']);
+        }
+
+        $studentData = $data['data_siswa'];
+        $courseData = $data['data_mapel'];
+
+        $dataMapel = \App\Models\Mapellm::get()->toArray();
+
+        foreach ($dataMapel as $value) {
+            $max_quota[$value['id']] = 0;
+            $max_total_class[$value['id']] = 0;
+        }
+
+        $record = [];
+        $className = "";
+
+        for ($i = 0; $i < count($studentData); $i++) {
+            if ($studentData[$i]['mapel_terpilih'] != 0) { // Hanya memproses siswa yang sudah ter-assign kelas
+                $idSelectedMapel = array_search($studentData[$i]['mapel_terpilih'], array_column($courseData, 'id'));
+                // $idSelectedMapel = $courseData[$idSelectedMapel][''];
+                if ($max_quota[$studentData[$i]['mapel_terpilih']] < $courseData[$idSelectedMapel]['max_kuota_kelas'] && $max_total_class[$studentData[$i]['mapel_terpilih']] < $courseData[$idSelectedMapel]['jumlah_kelas']) {
+                    // if ($max_quota[$studentData[$i]['mapel_terpilih']] == 0) {
+                    //     $className = $courseData[$idSelectedMapel]['nama_mapel'] . '_' . chr(65);
+                    // } else
+                    // \Illuminate\Support\Facades\Log::debug($idSelectedMapel);
+                    $className = $courseData[$idSelectedMapel]['nama_mapel'] . '_' . chr($max_total_class[$studentData[$i]['mapel_terpilih']] + 65);
+                    if ($max_quota[$studentData[$i]['mapel_terpilih']] % $courseData[$idSelectedMapel]['kuota_kelas'] == 4) {
+                        // $className = $courseData[$idSelectedMapel]['nama_mapel'] . '_' . chr($max_total_class[$studentData[$i]['mapel_terpilih']] + 65);
+                        $max_total_class[$studentData[$i]['mapel_terpilih']]++;
+                    }
+                    $max_quota[$studentData[$i]['mapel_terpilih']]++;
+                }
+
+                // Save data
+                // $value = [
+                //     'id_siswa' => $data[$i]['id'],
+                //     'id_mapellm' => $data[$i]['mapel_terpilih'],
+                //     'nama_kelas' => $this->className($data[$i]),
+                // ];
+            }
+            \Illuminate\Support\Facades\Log::debug($className);
+        }
+        // \App\Models\Siswa::insert($value);
+
+        // \Illuminate\Support\Facades\Log::debug($max_quota);
+        // \Illuminate\Support\Facades\Log::debug($max_total_class);
+        return response()->json(['errors' => [0 => 'Fail to update data']]);
+    }
+
+    private function className($data)
+    {
+        if (empty($data)) {
+            return json_encode(['fail' => 'Data inser is null']);
+        }
+
+        $dataMapel = \App\Models\Mapellm::get()->toArray();
+
+        foreach ($dataMapel as $value) {
+            $index[$value['id']] = 0;
+        }
+
+        foreach ($data as $value) {
+            $index[$value['mapel_terpilih']]++;
+        }
     }
 }
